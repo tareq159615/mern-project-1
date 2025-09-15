61bb69d2c51252ef02a66d026b03c6afe9ffe5fa
Tareq Aziz <125204331+tareq159615@users.noreply.github.com>
2025-09-15 07:25:27 +0600

    project 2nd  commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import {connectDB} from "./config/db.js";
+import { connectDB } from "./config/db.js";
 import path from "path";
 
 import productRoutes from "./routes/productRoute.js";
@@ -20,13 +20,13 @@ console.log(process.env.MONGO_URI);
 app.use("/api/products", productRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get(/(.*)/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
 }
 
-app.listen(PORT, () => {            
-    connectDB();
-    console.log("Server started at http://localhost:"+PORT);
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server started at http://localhost:" + PORT);
 });
